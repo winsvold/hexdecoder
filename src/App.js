2178f55e239f56146d4b160d79a49bb53609a89c
Ida Onshus <Ida.Onshus@nav.no>
2018-12-21 21:07:16 +0100

    adding barchart

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import './App.css';
 import HexInput from './components/HexInput';
 import BarChart from './components/BarChart';
 import hexToHsl from 'hex-to-hsl';
+import hexToRgb from 'hex-to-rgb';
 import HSLWheel from "./components/HSLWheel";
 
 function App() {
-  const [hexCode, setHexcode] = useState('');
-  const hsl = hexToHsl(hexCode);
-
-  return (
-    <div className="App">
-      <HexInput hexCode={hexCode} setHexcode={setHexcode}/>
-      <p>hue: {hsl[0]} saturation: {hsl[1]} lightness: {hsl[2]}</p>
-      <BarChart/>
-      <HSLWheel hexCode={hexCode}/>
-    </div>
-  );
+    const [hexCode, setHexcode] = useState('');
+    const hsl = hexToHsl(hexCode);
+    const rgb = hexToRgb(hexCode);
+    return (
+        <div className="App">
+            <HexInput hexCode={hexCode} setHexcode={setHexcode}/>
+            <p>hue: {hsl[0]} saturation: {hsl[1]} lightness: {hsl[2]}</p>
+            <BarChart rgb={rgb}/>
+            <HSLWheel hexCode={hexCode}/>
+        </div>
+     );
 }
 
 export default App;
--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,15 +1,40 @@
 import React from 'react';
+import './barchart.css';
 
+class BarChart extends React.Component {
 
-function BarChart() {
-    return (
-        <div className="bar">
-            <div class="r"></div>
-            <div class="g"></div>
-            <div class="b"></div>
-        </div>
-    )
+    calculateBarValue = (color) => {
+        const percent = Math.floor(color/255*100);
+        return 101 - percent;
+    };
 
+    render() {
+        const BarR = document.getElementById('Bar-r');
+        const BarG = document.getElementById('Bar-g');
+        const BarB = document.getElementById('Bar-b');
+
+        const r  = this.calculateBarValue(this.props.rgb[0]);
+        const g  = this.calculateBarValue(this.props.rgb[1]);
+        const b  = this.calculateBarValue(this.props.rgb[2]);
+
+        console.log(r,g,b);
+        if(BarR) {
+            BarR.style.setProperty('grid-row-start', r)
+        }
+        if(BarG) {
+            BarG.style.setProperty('grid-row-start', g)
+        }
+        if(BarB) {
+            BarB.style.setProperty('grid-row-start', b)
+        }
+        return (
+            <div className="BarChart">
+                <div id="Bar-r"></div>
+                <div id="Bar-g"></div>
+                <div id="Bar-b"></div>
+            </div>
+        )
+    }
 }
 
 export default BarChart;
