20c8114829471fe700b401a96cad7de1268e26d3
Ida Onshus <idaonshus@gmail.com>
2018-12-21 14:09:11 +0100

    adding barchart

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, {useState} from 'react';
 import './App.css';
 import HexInput from './components/HexInput';
+import BarChart from './components/BarChart';
 import hexToHsl from 'hex-to-hsl';
 
 function App() {
-  const [hexCode, setHexcode] = useState('');
-  const hsl = hexToHsl(hexCode);
-  console.log(hsl);
-  return (
-    <div className="App">
-      <HexInput hexCode={hexCode} setHexcode={setHexcode}/>
-      <p>hue: {hsl[0]} saturation: {hsl[1]} lightness: {hsl[2]}</p>
-    </div>
-  );
+    const [hexCode, setHexcode] = useState('');
+    const hsl = hexToHsl(hexCode);
+    console.log(hsl);
+    return (
+        <div className="App">
+            <HexInput hexCode={hexCode} setHexcode={setHexcode}/>
+            <p>hue: {hsl[0]} saturation: {hsl[1]} lightness: {hsl[2]}</p>
+            <BarChart/>
+        </div>
+     );
 }
 
 export default App;
---
+++ src/components/BarChart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function BarChart() {
+    return (
+        <div className="bar">
+            <div class="r"></div>
+            <div class="g"></div>
+            <div class="b"></div>
+        </div>
+    )
+
+}
+
+export default BarChart;
